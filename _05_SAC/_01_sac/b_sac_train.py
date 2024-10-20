1c5ee503b2f32e78dcbd080a65a1a6f4cb9f2deb
Youn-Hee Han <yh21.han@gmail.com>
2024-10-21 07:47:30 +0900

    ...

--- _05_SAC/_01_sac/b_sac_train.py
+++ _05_SAC/_01_sac/b_sac_train.py
@@ -312,10 +312,10 @@ def main() -> None:
     config = {
         "env_name": ENV_NAME,                               # 환경의 이름
         "max_num_episodes": 200_000,                        # 훈련을 위한 최대 에피소드 횟수
-        "batch_size": 256,                                  # 훈련시 배치에서 한번에 가져오는 랜덤 배치 사이즈
-        "steps_between_train": 32,                          # 훈련 사이의 환경 스텝 수
+        "batch_size": 512,                                  # 훈련시 배치에서 한번에 가져오는 랜덤 배치 사이즈
+        "steps_between_train": 16,                          # 훈련 사이의 환경 스텝 수
         "replay_buffer_size": 1_000_000,                    # 리플레이 버퍼 사이즈
-        "learning_rate": 0.0003,                            # 학습율
+        "learning_rate": 0.0001,                            # 학습율
         "gamma": 0.99,                                      # 감가율
         "soft_update_tau": 0.995,                           # Soft Update Tau
         "print_episode_interval": 20,                       # Episode 통계 출력에 관한 에피소드 간격
