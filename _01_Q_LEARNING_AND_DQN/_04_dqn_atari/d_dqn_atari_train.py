6879e50f5d9bec6b7682512258ccf85e6b276a6c
Youn-Hee Han <yh21.han@gmail.com>
2024-11-10 20:30:26 +0900

    ...

--- _01_Q_LEARNING_AND_DQN/_04_dqn_atari/d_dqn_atari_train.py
+++ _01_Q_LEARNING_AND_DQN/_04_dqn_atari/d_dqn_atari_train.py
@@ -40,7 +40,7 @@ def main():
 
     config = {
         "env_name": ENV_NAME,                             # 환경의 이름
-        "max_num_episodes": 2_000,                        # 훈련을 위한 최대 에피소드 횟수
+        "max_num_episodes": 10_000,                        # 훈련을 위한 최대 에피소드 횟수
         "batch_size": 32,                                 # 훈련시 배치에서 한번에 가져오는 랜덤 배치 사이즈
         "learning_rate": 0.00025,                         # 학습율
         "gamma": 0.99,                                    # 감가율
