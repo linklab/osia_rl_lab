a7b25aa0ce3c9ec946fd1dec21d1789807c2e990
Youn-Hee Han <yh21.han@gmail.com>
2024-11-10 15:12:26 +0900

    ...

--- _01_Q_LEARNING_AND_DQN/_03_dqn_atari/d_dqn_train.py
+++ _01_Q_LEARNING_AND_DQN/_03_dqn_atari/d_dqn_train.py
@@ -263,13 +263,13 @@ def main() -> None:
 
     config = {
         "env_name": ENV_NAME,                             # 환경의 이름
-        "max_num_episodes": 2_000,                      # 훈련을 위한 최대 에피소드 횟수
-        "batch_size": 32,                                # 훈련시 배치에서 한번에 가져오는 랜덤 배치 사이즈
-        "learning_rate": 0.00025,                          # 학습율
+        "max_num_episodes": 2_000,                        # 훈련을 위한 최대 에피소드 횟수
+        "batch_size": 32,                                 # 훈련시 배치에서 한번에 가져오는 랜덤 배치 사이즈
+        "learning_rate": 0.00025,                         # 학습율
         "gamma": 0.99,                                    # 감가율
         "steps_between_train": 2,                         # 훈련 사이의 환경 스텝 수
-        "target_sync_step_interval": 10_000,               # 기존 Q 모델을 타깃 Q 모델로 동기화시키는 step 간격
-        "replay_buffer_size": 200_000,                  # 리플레이 버퍼 사이즈
+        "target_sync_step_interval": 500,                 # 기존 Q 모델을 타깃 Q 모델로 동기화시키는 step 간격
+        "replay_buffer_size": 200_000,                    # 리플레이 버퍼 사이즈
         "epsilon_start": 0.99,                            # Epsilon 초기 값
         "epsilon_end": 0.01,                              # Epsilon 최종 값
         "epsilon_final_scheduled_percent": 0.75,          # Epsilon 최종 값으로 스케줄되는 마지막 에피소드 비율
